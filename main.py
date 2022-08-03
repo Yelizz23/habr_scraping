ee55683c7c3f56e5bc4797fa3977161fee90eb40
Eleonora <103573450+Yelizz23@users.noreply.github.com>
2022-08-03 16:59:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,8 @@ if __name__ == '__main__':
     item = habr.Habr()
     print('Поиск по всей доступной preview-информации:')
 
-    # Поиск по ключевым словам в превью статьи
     item.preview_info(KEY_WORDS)
     print('\n')
     print('Поиск целиком по всей статье:')
 
-    # Поиск по ключевым словам в основном тексте статьи
     item.article_text(KEY_WORDS)
